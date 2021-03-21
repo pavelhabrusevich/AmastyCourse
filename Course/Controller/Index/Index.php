22495b493842be001ca86cf8945507b642cb6ffc
gabrusevich <pavel.gabrusevich@amasty.com>
2021-03-21 15:03:33 +0300

    [Amasty-Training] task-3. Module config is added. Config-Front dependencies are added

--- Course/Controller/Index/Index.php
+++ Course/Controller/Index/Index.php
@@ -3,12 +3,31 @@
 namespace Amasty\Course\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        parent::__construct($context);
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function execute()
     {
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        if ($this->scopeConfig->isSetFlag('am_course_config/general/enabled')) {
+            return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        } else {
+            die('Module is disabled');
+        }
     }
 }
